7cf8f145033bc4e22b7bf2f87ff890cfa8c033c9
TBExtent <jedpreist@gmail.com>
2023-05-07 15:16:44 +0100

    count von count time

---
+++ testcounter.hpp
@@ -0,0 +1,38 @@
+
+
+#ifndef COUNTER_H
+#define COUNTER_H
+
+#include "sensor.hpp"
+
+namespace Sensor {
+    class CounterSensor : public Sensor {
+    private:
+        double count;
+    public:
+        CounterSensor(uint8_t pin);
+        ~CounterSensor();
+
+        void tick();
+        double read();
+    };
+
+    CounterSensor::CounterSensor(uint8_t pin) {
+        count = 0;
+    }
+
+    CounterSensor::~CounterSensor() {
+
+    }
+
+    void CounterSensor::tick() {
+
+    }
+
+    double CounterSensor::read() {
+        count++;
+        return count;
+    }
+}
+
+#endif
